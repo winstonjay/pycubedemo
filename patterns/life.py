1c8b50f9b200935fd8803ae72a49fb02fccc5466
Karl Sims <karlsims.uk@gmail.com>
2017-09-25 05:26:48 +0100

    ordered orginal version

--- patterns/life.py
+++ patterns/life.py
@@ -2,69 +2,78 @@
 Author:   Karl Sims. 2017.
 Goal:     Implement 3D version of Conway's Game of Life for LED cubes.
 
-python cube.py -P cuboid:3000 --pattern life
-
-Version 3
+ORIGNAL Version.
 """
 import random
 
 
 class Pattern(object):
+    """Implement a pattern for led cube framework.
+    self.cube.clear, self.cube.set_pixel are made avalible in time
+    when Pattern instance is created in cube.py."""
 
     def init(self):
-        self.double_buffer = True # dosent clear propperly.
-        self.life = CubeLife(size=self.cube.size, 
-                             limit=100, 
-                             new_state=random_blast,
-                             next_generation=next_generation_3d)
+        self.double_buffer = True # Stop flickering bewteen ticks.
+        self.life = Life(size=self.cube.size, 
+                         limit=100, 
+                         new_state_fn=random_blast,
+                         next_generation_fn=next_generation_3d)
         return 1.0/8 # time between ticks.
 
     def tick(self):
+        "What to do every iteration."
         self.cube.clear()
-        for (x, y, z), color in self.life.successors():
-            self.cube.set_pixel((x, y, z), color)
+        for xyz_position, color in self.life.successors():
+            self.cube.set_pixel(xyz_position, color)
 
 
 
 
-class CubeLife(object):
+class Life(object):
     "Implements interface for generating successor states in conways game of life."
     
-    def __init__(self, size, limit, new_state, next_generation, limited=False):
+    def __init__(self, size, limit, new_state_fn, next_generation_fn, limited=False):
         self.size = size
         self.limit = limit
-        # Add generation functions.
-        self.new_state = new_state
-        self.next_gen = next_generation
-        # set initial values.
+        self.limited = False
+        self.new_state_fn = new_state_fn
+        self.next_generation_fn = next_generation_fn
+        self.color_change = self.__color_change_2dim_secondary
+        # self.color_change = self.__color_change
+        # self.color_change = self.__color_dim
+        # self.color_change = self.__color_change_2black
         self.__restart()
 
     def successors(self):
-        for position, mutation in next(self.__life()):
-            yield ((position, self.color2) if mutation else
-                   (position, self.color1))
+        generations = self.__life()
+        for (position, rgb) in next(generations).items():
+            yield (position, rgb)
 
     def __life(self):
-        "yield whole generations of life forever."
         while True:
             if self.__done():
                 self.__restart()
             else:
-                self.state = self.next_gen(self.state, self.size)
                 self.count += 1
-                self.patterns.append(self.state)
-            yield self.state
+                self.state = self.next_generation_fn(self.state, self.size)
+            self.patterns.append(self.state)
+            self.color_change()
+            for k in self.state:
+                self.visited[k] = self.color1
+            yield self.visited
 
     def __restart(self):
-        "Set initial values for each new game of life"
         self.count = 0
+        self.visited = dict()
         self.patterns = []
         self.state = set()
         self.color1, self.color2 = new_colors()
-        # Set the inital state from which to spawn Life.
-        self.state = self.new_state(self.size)
+        self.cDeltas = color_differences(self.color1, self.color2, 10)
+        self.state = self.new_state_fn(self.size)
 
     def __done(self):
+        if self.limited and self.count == self.limit:
+            raise StopIteration
         if len(self.patterns) >= 6:
             self.patterns.pop(0)
             # We dont want to be stuck on short repeating patterns forever.
@@ -73,11 +82,59 @@ class CubeLife(object):
                 return True
         return self.count == self.limit
 
+    """
+    TODO: deside color change function for good.
+    
+    NOTES:
+    __color_change:        
+        probs a bit mental and random looking. works good in 2d.
+        kinda hides the the fact its life. but i guess you wont see
+        that here anyway.
+    __color_change_2dim_secondary:
+        Kinda nice and chill not as mental as __color_change.
+    __color_dim:
+        kinda nice and chill.
+    __color_change_2black: 
+        probs a bit boring. if used can streamline visited.
+    
+    Replace self.color_change in __init__ with a color change function of choice.
+    """
+
+    def __color_change(self):
+        """Subtract cDeltas from visited colors to do a pesudo transition
+        to secondary color. create echo effect to keep constant motion.
+        This is done by Allowing colors decrement/increment till they
+        reach the limit of valid rgb values then switch them to the set
+        secondary color, this loop repeats till visited is reset."""
+        dr, dg, db = self.cDeltas
+        c2 = self.color2
+        for k, (r, g, b) in self.visited.items():
+            (r, g, b) = (r-dr, g-dg, b-db)
+            new_rgb = tuple([c2[i] if c > 205 else c2[i] if c < 20 else 
+                            int(c) for i, c in enumerate((r, g, b))])
+            self.visited[k] = new_rgb
+
+    def __color_change_2black(self):
+        "just turn all visited squares to black."
+        for k, value in self.visited.items():
+            self.visited[k] = (0,0,0)
+
+    def __color_dim(self):
+        "fade out visited co-ords"
+        (r1, g1, b1) = self.color2
+        for k, (r, g, b) in self.visited.items():
+            self.visited[k] = (r//2, g//2, b//2)
+
+    def __color_change_2dim_secondary(self):
+        "fade out visited co-ords"
+        (r1, g1, b1) = self.color2
+        for k, (r, g, b) in self.visited.items():
+            self.visited[k] = (r1//8, g1//8, b1//8)
+
 
 
 """
-Life implementation in 3d.
-
+Basic Life implementation in 3d.
 Fundamental 'Life' Rules in square land:
 1) Any live cell with fewer than two live neighbors dies, 
    as if caused by underpopulation.
@@ -87,14 +144,11 @@ Fundamental 'Life' Rules in square land:
    to the next generation.
 4) Any dead cell with exactly three live neighbors becomes 
    a live cell.
-
 More info: http://web.stanford.edu/~cdebs/GameOfLife/
-
 Another's ideas about creating versions in cube land:
 http://www.complex-systems.com/pdf/01-3-1
 Current neighbour rules are tentative. Tweaking the inital state function
 may also be helpful in ensuring a got life / failed-life ratio.
-
 TODO: choose for good neighbour rules.
 """
 
@@ -105,22 +159,19 @@ def next_generation_3d(state, size=None):
     new_state = set()
     for cell in state:
         cell_neighbours = neighbours_3d(cell)
-        if len(state & cell_neighbours) in (3, 4): # rule 3 / (1 & 2)
+        if len(state & cell_neighbours) in (3, 4, 5): # rule 3 / (1 & 2)
             new_state.add(cell)
-        elif len(state & cell_neighbours) is 5: # rule 6 they get sick
-            c, _ = cell
-            new_state.add((c, True))
-        for cn in cell_neighbours: # rule 4
-            if len(state & neighbours_3d(cn)) is 5: 
+        for cn in cell_neighbours:
+            if len(state & neighbours_3d(cn)) is 5: # rule 4
                 new_state.add(cn)
-    return (new_state if not size else constrain(new_state, size))
+    return (new_state if not size else constrain_3d(new_state, size))
 
 
 def neighbours_3d(cell):
     """neighbours(tuple: cell): set: {(ints: x, y, z), ...};
     returns a set of co-ordinates +/- 1 from a given position."""
-    (x, y, z), mute = cell
-    return set(((x + dx, y + dy, z + dz), mute) for dx, dy, dz in deltas_3d)
+    (x, y, z) = cell
+    return set((x + dx, y + dy, z + dz) for dx, dy, dz in deltas_3d)
 
 
 deltas_3d = [(x, y, z) for x in range(-1, 2) 
@@ -129,33 +180,37 @@ deltas_3d = [(x, y, z) for x in range(-1, 2)
                        if not (x == y == z == 0)]
 
 
-def constrain(S, s):
-    return set(((x, y, z), mute) for (x, y, z), mute in S
-               if 0 <= x < s and 0 <= y < s and 0 <= z < s)
-
-
 # Random Utilies / Initial State funcitons:
 
 __rand = random.randint
 
+def random_state(size):
+    "return x, y, z co-ords that could be anywhere in state space."
+    return set((__rand(0,size-1), __rand(0,size-1), __rand(0,size-1)) 
+                for x in range(__rand(size, size*3)))
+
+def constrained_random_state(size):
+    "return x, y, z co-ords limiting to smaller part of the state space."
+    edge = int(size/2.2)
+    return set((__rand(edge, size - edge), 
+                __rand(edge, size - edge),
+                __rand(edge, size - edge))
+                for x in range(8))
+
+def random_state3d(size):
+    "return x, y, z co-ords with +-1 to increase changes of starting life"
+    return set((x+d, y+d, z+d) for d in range(-1, 2) 
+               for x, y, z in constrained_random_state(size)) | random_state(size)
+
 def random_blast(size):
     "concentrated center scattered rest."
-    edge = int(size/2.2)
-    bundle = set(((__rand(edge, size - edge-1), 
-                   __rand(edge, size - edge-1),
-                   __rand(edge-1, size - edge)), False)
-                 for position in range(8))
-    scatter = set(((__rand(0,size-1), 
-                    __rand(0,size-1), 
-                    __rand(0,size-1)), False) 
-                  for x in range(__rand(size, size*3)))
-    return (bundle | scatter)
+    return random_state3d(size) | random_state(size)
 
 
 # Color Utilies:
 
 def new_colors():
-    "return 2 random rgb colors from material design colors"
+    "return 2 random rgb color from material design colors"
     return random.choice(material_colors)
 
 material_colors = [
@@ -171,4 +226,12 @@ material_colors = [
     ((0,191,165), (98,0,234)),     # dark teal deep purple
 ]
 
+def color_differences(c1, c2, steps):
+    "Calculate step size for a given amount of steps between 2 rgb colors"
+    (r1, g1, b1), (r2, g2, b2) = (c1, c2)
+    return ((r1 - r2)/steps, (g1 - g2)/steps, (b1 - b2)/steps)
 
+def constrain_3d(S, s):
+    "Make little s within big S and 0"
+    return set((x, y, z) for x, y, z in S 
+               if 0 <= x < s and 0 <= y < s and 0 <= z < s)
